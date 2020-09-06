2f3dbf1bf674a1340534b8fb019aec269e55c2e9
Santiago Degetau Sada <santiago.degetau@icloud.com>
2020-09-06 13:13:50 -0600

    Fix checkbox issues (missing: jump to top on click)

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,28 +1,24 @@
-import React, { ChangeEvent, useState } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { colors } from "styles/colors";
 
-export default (props: RangeProps) => {
+export default (props: CheckboxProps) => {
   const [mChecked, mSetChecked] = useState(props.checked || false);
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, checked } = e.currentTarget;
+  const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.preventDefault();
+    const checked = !mChecked;
     if (props.onChange != null) {
-      props.onChange(name, checked);
+      props.onChange(props.name, checked);
     }
     mSetChecked(checked);
   };
 
   return (
-    <StyledCheckbox className={`checkbox form-item ${mChecked && "checked"}`}>
-      <input
-        type="checkbox"
-        id={props.name}
-        name={props.name}
-        value={props.name}
-        checked={mChecked}
-        onChange={handleChange}
-      />
+    <StyledCheckbox
+      className={`checkbox form-item ${mChecked ? "checked" : ""}`}
+      onClick={handleClick}
+    >
       <span className="checkmark"></span>
       {props.label && <label htmlFor={props.name}>{props.label}</label>}
     </StyledCheckbox>
@@ -33,29 +29,10 @@ const StyledCheckbox = styled.div`
   position: relative;
   transition: all 0.3s;
 
-  &:hover input ~ .checkmark {
-    background-color: ${colors.bg.light_hover};
-  }
-
-  input {
-    position: absolute;
-    cursor: pointer;
-    height: 100%;
-    width: 100%;
-    outline: none;
-    appearance: none;
-    margin: 0;
-    padding: 0;
-
-    &:checked ~ .checkmark {
-      background-color: ${colors.blue.base};
-      border-radius: 1px;
-      box-shadow: none;
-    }
-
-    &:checked ~ .checkmark:after {
-      display: block;
-    }
+  label {
+    display: block;
+    position: relative;
+    overflow: hidden;
   }
 
   .checkmark {
@@ -66,7 +43,6 @@ const StyledCheckbox = styled.div`
     background-color: ${colors.bg.light};
 
     &:after {
-      pointer-events: none;
       content: "";
       position: absolute;
       display: none;
@@ -79,9 +55,31 @@ const StyledCheckbox = styled.div`
       transform: rotate(45deg);
     }
   }
+
+  &.checked {
+    .checkmark {
+      background-color: ${colors.blue.base};
+      border-radius: 1px;
+      &:after {
+        display: block;
+      }
+    }
+    &:hover {
+      .checkmark {
+        background-color: ${colors.blue.dark};
+      }
+    }
+  }
+
+  &:hover {
+    cursor: pointer;
+    .checkmark {
+      background-color: ${colors.bg.light_hover};
+    }
+  }
 `;
 
-type RangeProps = {
+type CheckboxProps = {
   name: string;
   checked?: boolean;
   label?: JSX.Element | string;
